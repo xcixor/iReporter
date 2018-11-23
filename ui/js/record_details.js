deb691abde72ff277fdcaebc5d1e917a7c7362b5
Peter <pndungu54@gmail.com>
2018-11-23 05:52:50 +0300

    Feat - Edit record
    -Add form to edit record
    
    -Add styling to page
    
    -Add placeholder text
    
    [Finishes #162113014]

---
+++ ui/js/record_details.js
@@ -0,0 +1,1 @@
+document.getElementById('description').innerHTML = 'Women of a certain tribe are not treated in this hospital. This matter has been going on for several months now. Something very seriousl needs to be done. Our women are in jeopardy';
