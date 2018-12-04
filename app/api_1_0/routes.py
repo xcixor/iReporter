0679c54d0d325df5f25d23fa1861ed6217e9059a
Peter <pndungu54@gmail.com>
2018-12-04 08:27:18 +0300

    [Finishes #162405233] Recover files for reverted story #162399133

--- app/api_1_0/routes.py
+++ app/api_1_0/routes.py
@@ -3,12 +3,12 @@
 from app.api_1_0 import api
 from app.api_1_0 import views
 
-api.add_resource(views.Incident, '/incidents')
-api.add_resource(views.IncidentManipulation, '/incidents/<incident_id>')
-api.add_resource(views.EditIncidentComment,
-                 '/incidents/<incident_id>/comments')
-api.add_resource(views.EditIncidentLocation,
-                 '/incidents/<incident_id>/location')
+api.add_resource(views.RedFlag, '/redflags')
+api.add_resource(views.RedFlagManipulation, '/redflags/<redflag_id>')
+api.add_resource(views.EditRedFlagComment,
+                 '/redflags/<redflag_id>/comments')
+api.add_resource(views.EditRedFlagLocation,
+                 '/redflags/<redflag_id>/location')
 api.add_resource(views.Signup, '/auth/signup')
 api.add_resource(views.Signin, '/auth/login')
 api.add_resource(views.Signout, '/auth/logout/<user_id>')
