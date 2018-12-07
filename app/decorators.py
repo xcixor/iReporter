f0a2452edc04d090d575a36edb734a838cdf6677
Peter <pndungu54@gmail.com>
2018-12-07 07:40:16 +0300

    Fix unresponsive decorators

--- app/decorators.py
+++ app/decorators.py
@@ -1,21 +1,16 @@
 """Defines the decorators for the errors."""
-from flask import Blueprint, make_response, jsonify
+from flask import Blueprint, jsonify
 
 errors = Blueprint('errors', __name__)
 
 
-@errors.errorhandler(404)
+@errors.app_errorhandler(404)
 def resource_not_found(e):
     """Show custom error for inexisting resource."""
     return jsonify({"error": "Resource not found"}), 404
 
-@errors.errorhandler(500)
+
+@errors.app_errorhandler(500)
 def internal_server_error(e):
     """Show custom error for inexisting resource."""
     return jsonify({"error": "Internal Server error"}), 500
-
-
-@errors.errorhandler(405)
-def method_not_allowed(e):
-    """Show custom error for inexisting resource."""
-    return jsonify({"error": "Internal Server error"}), 405
--- instance/config.py
+++ instance/config.py
@@ -7,18 +7,12 @@ class Config:
     """Contains the basic settings for all configurations."""
 
     debug = False
-    SECRET_KEY = os.urandom(30)
-
-    @staticmethod
-    def init_app(app):
-        """To perform configuration specific initializations."""
-        pass
 
 
 class Development(Config):
     """Contains configurations to be used by developer."""
 
-    DEBUG = True
+    DEBUG = False
 
 
 class Testing(Config):
@@ -31,6 +25,7 @@ class Production(Config):
     """Contains configurations for production setting."""
 
     DEBUG = False
+    PROPAGATE_EXCEPTIONS = True
 
 
 # Register the configurations
